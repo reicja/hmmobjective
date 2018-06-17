f75422c5c7dd05a6af9f19dd735d8ac78043f349
reicja <reichl.hon@gmail.com>
2018-06-17 15:50:05 +0200

    ARHMM commit

--- hmm_objective_test.py
+++ hmm_objective_test.py
@@ -9,7 +9,7 @@ Created on Tue May 15 18:46:38 2018
 import numpy as np
 from scipy.stats import norm
 from scipy.stats import t as student
-from hmm_objective import GHMM, THMM
+from hmm_objective import GHMM, THMM, ARHMM
 from hmm_objective import sample_hmm
 from scipy import signal
 import matplotlib.pyplot as plt
@@ -26,13 +26,34 @@ t_pdfs = lambda pars, n_states: [student(loc=pars['loc'][k],
                                          df=pars['df'][k])
                                  for k in range(n_states)]
 
+
+def sample_ar1hmm(prior, transmat, phi, loc, scale, n_samples):
+    n_states = len(prior)
+    obs = np.zeros(shape=(n_samples, 1))
+    states = [np.random.choice(n_states, size=1, p=prior)[0]]
+    obs_init = np.random.normal(size=n_states)
+
+    obs[0] = norm(loc=(loc[states[0]] +
+                       np.dot(phi[states[0]], obs_init[states[0]])),
+                  scale=scale[states[0]]).rvs()
+
+    for n in range(1, n_samples):
+        states.append(np.random.choice(n_states, size=1,
+                                       p=transmat[states[-1], :])[0])
+        obs[n] = norm(loc=(loc[states[-1]] + np.dot(phi[states[-1]], obs[-1])),
+                      scale=scale[states[-1]]).rvs()
+    return obs, states
+
+
 n_samples = 800
 prior = np.array([.1, .85, .05])
 
 a11, a12, a13 = .75, .2, .05
 a21, a22, a23 = .07, .9, .03
 a31, a32, a33 = .05, .05, .9
-transmat = np.array([[a11, a12, a13], [a21, a22, a23], [a31, a32, a33]])
+transmat = np.array([[a11, a12, a13],
+                     [a21, a22, a23],
+                     [a31, a32, a33]])
 
 params_norm = {}
 params_norm['loc'] = [-.1, .05, .9]
@@ -75,6 +96,29 @@ print "Simulated data likelihood: -{0}".format(
                   params_t['df'],
                   params_t['loc'],
                   params_t['scale']))))
+
+
+phi = np.array([.05, -.1, .9])
+loc = np.array([-.3, 0, .3])
+scale = np.array([.1, .7, .9])
+obs, states = sample_ar1hmm(prior=prior,
+                            transmat=transmat,
+                            phi=phi,
+                            loc=loc,
+                            scale=scale,
+                            n_samples=800)
+
+hmm = ARHMM(n_states=3)
+# parameters clip
+# scale, loc, phi
+lb = np.array([0, 0, 0, -2, -2, -2, -1, -1, -1])
+ub = np.array([3, 2, 1, 2, 2, 2, 1, 1, 1])
+hmm.fit(obs, lb, ub)
+print "Simulated data likelihood: -{0}".format(
+        hmm._cost(obs, np.hstack((prior, transmat.flatten(),
+                  scale,
+                  loc,
+                  phi))))
 
 
 # wawelet analysis
